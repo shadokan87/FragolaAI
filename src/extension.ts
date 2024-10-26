8285e842036e17b7d32a6d65648b483f1edec05e
shadokan87 <eclipse.toure@outlook.fr>
2024-10-26 21:02:53 +0200

    fixed tsconfig noEmit error

--- src/extension.ts
+++ src/extension.ts
@@ -16,11 +16,34 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('fragola-ai.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fragolaAI!');
+		// vscode.window.showInformationMessage('Hello World from fragolaAI!');
+		const panel = vscode.window.createWebviewPanel(
+			'catCoding',
+			'Cat Coding',
+			vscode.ViewColumn.One,
+			{}
+		  );
+	
+		  // And set its HTML content
+		  panel.webview.html = getWebviewContent();
 	});
 
 	context.subscriptions.push(disposable);
 }
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <body>
+	  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+  </body>
+  </html>`;
+  }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
